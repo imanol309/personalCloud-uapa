a39dd28c039bbeac901d8c381dd980c8f152b3df
imanol309 <imanolguzman309@gmail.com>
2024-01-03 01:09:33 -0400

    Agregando configuracion para favicon

--- personalcloud-front/src/app/layout.js
+++ personalcloud-front/src/app/layout.js
@@ -14,6 +14,13 @@ export default function RootLayout({ children }) {
       <html lang="es">
         <Head>
           <link rel="shortcut icon" href="favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="manifest" href="/site.webmanifest"/>
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#30235b"/>
+          <meta name="msapplication-TileColor" content="#30235b"/>
+          <meta name="theme-color" content="#ffffff"></meta>
         </Head>
         <body className={`${rubik.className} antialiased page`}>{children}</body>
       </html>

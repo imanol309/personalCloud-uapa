fd174e4e4f37702964905d3bdd172f1d88be4d8b
imanol309 <imanolguzman309@gmail.com>
2024-01-10 02:01:36 -0400

    Agregando modal de visualizar imagen

--- personalcloud-front/src/app/ui/cardfile/CardFile.jsx
+++ personalcloud-front/src/app/ui/cardfile/CardFile.jsx
@@ -1,8 +1,6 @@
 "use client";
 import Image from "next/image";
 import "./CardFile.css";
-// import iconPdf from "../../../../public/icons/type/iconPdf.png";
-// import iconImage from "../../../../public/icons/type/iconImage.png";
 import viewIcon from "../../../../public/icons/icons8-panel-de-vista-previa-50.png";
 import downloadIcon from "../../../../public/icons/icons8-descargar-26.png";
 
@@ -12,10 +10,13 @@ import pdf from "../../../../public/icons/type/pdf.svg";
 import txt from "../../../../public/icons/type/txt.svg";
 import xlsx from "../../../../public/icons/type/xlsx.svg";
 import { useState } from "react";
+import PureModal from "react-pure-modal";
+import "react-pure-modal/dist/react-pure-modal.min.css";
+
 
 export const CardFile = ({ viewMode, data }) => {
   const [imageBlob, setImageBlob] = useState(null);
-  console.log(data);
+  const [modal, setModal] = useState(false)
   const downloadFile = async () => {
     try {
       const response = await fetch(data.link);
@@ -30,6 +31,7 @@ export const CardFile = ({ viewMode, data }) => {
       console.error("Error al descargar la imagen:", error);
     }
   };
+
   const officers = {
     txt: txt,
     doc: doc,
@@ -37,38 +39,85 @@ export const CardFile = ({ viewMode, data }) => {
     peg: pngImg,
     png: pngImg,
     jpg: pngImg,
-    lsx: xlsx
+    lsx: xlsx,
   };
 
-  const ifType =
-    data.type === "peg" || data.type === "png" || data.type === "jpg"
-      ? pngImg
-      : data.type === "doc"
-      ? doc
-      : pdf;
+  const modalBody = () => (
+    <div
+        style={{
+            backgroundColor: '#fff',
+
+            /* Fixed position */
+            left: 0,
+            position: 'fixed',
+            top: 0,
+
+            /* Take full size */
+            height: '100%',
+            width: '100%',
+
+            /* Displayed on top of other elements */
+            zIndex: 9999,
+
+            display: 'flex',
+            flexDirection: 'column',
+            overflow: 'hidden',
+        }}
+    >
+        {/* <!-- We will display viewer here --> */}
+        <Viewer fileUrl={`${data.link}`} />
+    </div>
+);
 
   return (
-    <div className={viewMode ? "CardFile--Grid" : "CardFile--List"}>
-      <div className={viewMode ? "CardFile__Box--Grid" : "CardFile__Box--List"}>
-        <Image
-          src={officers[data.type]}
-          className={viewMode ? "CardFile__Icon--Grid" : "CardFile__Icon--List"}
-          alt="Icon Image"
-          height={35}
-          width={35}
-        />
-        <p className="CardFile__Title">{data.name_file}</p>
+    <>
+      <PureModal
+        className=""
+        style={{height: "50%"}}
+        width="auto"
+        isOpen={modal}
+        closeButtonPosition="bottom"
+        onClose={() => {
+          setModal(false);
+          return true;
+        }}
+      >
+        <div className="">
+          <div className="">
+            <img
+              className=""
+              src={data.link}
+              alt={data.name_file}
+            />
+          </div>
+        </div>
+      </PureModal>
+      <div className={viewMode ? "CardFile--Grid" : "CardFile--List"}>
+        <div
+          className={viewMode ? "CardFile__Box--Grid" : "CardFile__Box--List"}
+        >
+          <Image
+            src={officers[data.type]}
+            className={
+              viewMode ? "CardFile__Icon--Grid" : "CardFile__Icon--List"
+            }
+            alt="Icon Image"
+            height={35}
+            width={35}
+          />
+          <p className="CardFile__Title">{data.name_file}</p>
+        </div>
+        <p className="CardFile__Date">{data.date}</p>
+        <div className="CardFile__Icons">
+          <Image
+            src={downloadIcon}
+            alt="Icon More"
+            onClick={downloadFile}
+            className="IconDown"
+          />
+          <Image src={viewIcon} alt="Icon More" className="IconDown" onClick={() => setModal(true)} />
+        </div>
       </div>
-      <p className="CardFile__Date">{data.date}</p>
-      <div className="CardFile__Icons">
-        <Image
-          src={downloadIcon}
-          alt="Icon More"
-          onClick={downloadFile}
-          className="IconDown"
-        />
-        <Image src={viewIcon} alt="Icon More" className="IconDown" />
-      </div>
-    </div>
+    </>
   );
 };
